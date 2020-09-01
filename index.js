e59c3bdde343da2caf15a9156db40e80a1e23421
SanjuB89 <sanju_benz@outlook.com>
2020-09-01 10:03:57 -0700

    adds github

--- index.js
+++ index.js
@@ -4,6 +4,9 @@ const server = express();
 server.get("/", (req, res) => {
   res.send("Welcome Home");
 });
+server.get("/github", (req, res) => {
+  res.send("Welcome to github");
+});
 
 //if PORT dont exist add 3000
 let PORT = process.env.PORT || 3000;
