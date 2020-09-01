ed36a54e07fd4d7cab29c60124771e60425eebdc
SanjuB89 <sanju_benz@outlook.com>
2020-09-01 09:18:13 -0700

    changed port assignment

--- index.js
+++ index.js
@@ -4,6 +4,9 @@ const server = express();
 server.get("/", (req, res) => {
   res.send("Welcome Home");
 });
-server.listen(3000, () => {
+
+//if PORT dont exist add 3000
+let PORT = process.env.PORT || 3000;
+server.listen(PORT, () => {
   console.log("server listening");
 });
